b4e70bc6e5b59d7e25b9110788c8e8ca1c76c9ca
nihalathikkai <nihalathikkai1999@gmail.com>
2022-05-17 23:01:45 +0530

    Learn pyspark Day 2
    17-05-2022

--- rdd/nasaApacheWebLogs/SameHostsProblem.py
+++ rdd/nasaApacheWebLogs/SameHostsProblem.py
@@ -1,8 +1,7 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
-
-	'''
+    '''
     "in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
     "in/nasa_19950801.tsv" file contains 10000 log lines for August 1st, 1995
     Create a Spark program to generate a new RDD which contains the hosts which are accessed on BOTH days.
@@ -18,3 +17,17 @@ if __name__ == "__main__":
 
     Make sure the head lines are removed in the resulting RDD.
     '''
+        
+    conf = SparkConf().setAppName("unionLogs").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+    
+    logs1 = sc.textFile("in/nasa_19950701.tsv")
+    logs2 = sc.textFile("in/nasa_19950801.tsv")
+    
+    logs1host = logs1.filter(lambda x : not x.startswith("host\tlogname")).map(lambda x : x.split("\t")[0])
+    logs2host = logs2.filter(lambda x : not x.startswith("host\tlogname")).map(lambda x : x.split("\t")[0])
+    logsunion = logs1host.union(logs2host)
+    
+    logsunion.saveAsTextFile("out/nasa_logs_same_hosts.csv")
+    
+    sc.stop()
